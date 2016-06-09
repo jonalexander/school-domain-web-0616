f98d981a4d97c6468d8774de67fddab5a4b1d58c
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-09 17:48:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+require 'Pry'
+
+class School
+
+  attr_accessor :name
+  #attr_reader
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, age)
+    @roster[age] ||= []
+    @roster[age] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    #sorts all keys
+    @roster.sort.to_h
+    #sorts all values and reassigns key/value pairs
+    @roster.each do |grade, student_array|
+      @roster[grade] = student_array.sort
+    end
+  end
+end
+
+#Pry.start
+
+# expected:   {10=>["Jeff Baird", "Blake Johnson"]}
+# expected:   {9=>["Homer Simpson"], 10=>["Jeff Baird", "Avi Flombaum"], 7=>["Blake Johnson"]}
